5c02ca2629b699a507a63c140eb69a7dfe6407f5
Kavyansh Pancholi <kavyanshpancholi@gmail.com>
2024-05-23 11:51:05 +0530

    feat: header, hero section header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,22 @@
+import Header from "./components/Header";
+
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">zipd</h1>
+      <Header />
+      <div className="flex flex-col mt-20 items-center gap-3 font-semibold">
+        <h1 className="text-5xl lowercase">
+          From Long
+          <span className="uppercase"> URL</span>s to{" "}
+        </h1>
+        <h1 className="text-5xl lowercase">
+          <span className="tracking-wide text-yellow-400">
+            zipd
+          </span>{" "}
+          Links
+        </h1>
+      </div>
     </>
   );
 }
