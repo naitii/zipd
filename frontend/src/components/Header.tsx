27b5b7a54fa65ee2c2198cd5629f62318980beb0
Chirayu31 <chirayupancholi17@gmail.com>
2024-05-23 15:46:53 +0530

    feat: added shadcn with light/dark mode dropdown menu and button

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import { ModeToggle } from './mode-toggle'
 
 function Header() {
-    return (
-        <header className='flex mx-10 mt-5 justify-between'>
-            <div className="logo">
-                <h2 className='text-3xl font-semibold tracking-wide text-yellow-400'>zipd</h2>
-            </div>
-            <div className="auth-buttons flex gap-5">
-                <button className="btn font-medium text-lg ">Log In</button>
-                <button className="btn font-medium text-lg ">Sign Up</button>
-            </div>
-        </header>
-    );
+  return (
+    <header className='flex mx-10 mt-5 justify-between'>
+      <div className='logo'>
+        <h2 className='text-3xl font-semibold tracking-wide text-yellow-400'>
+          zipd
+        </h2>
+      </div>
+      <div className='auth-buttons flex gap-5'>
+        <ModeToggle />
+        <button className='btn font-medium text-lg '>Log In</button>
+        <button className='btn font-medium text-lg '>Sign Up</button>
+      </div>
+    </header>
+  )
 }
 
-export default Header;
+export default Header
