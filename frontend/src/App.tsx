9f23b8f1d48bcbe0f97b37b63d53f3f2122da83a
anshttg <pancholiansh17@gmail.com>
2024-05-23 16:58:24 +0530

    router-integration-modified

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,18 +12,6 @@ function App() {
   return (
     <>
       <RouterProvider router={router}/>
-      <div className="flex flex-col mt-20 items-center gap-3 font-semibold">
-        <h1 className="text-5xl lowercase">
-          From Long
-          <span className="uppercase"> URL</span>s to{" "}
-        </h1>
-        <h1 className="text-5xl lowercase">
-          <span className="tracking-wide text-yellow-400">
-            zipd
-          </span>{" "}
-          Links
-        </h1>
-      </div>
     </>
   );
 }
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,7 +2,21 @@ import Header from '@/components/Header'
 
 const Home = () => {
   return (
-    <Header/>
+    <>
+      <Header/>
+      <div className="flex flex-col mt-20 items-center gap-3 font-semibold">
+          <h1 className="text-5xl lowercase">
+            From Long
+            <span className="uppercase"> URL</span>s to{" "}
+          </h1>
+          <h1 className="text-5xl lowercase">
+            <span className="tracking-wide text-yellow-400">
+              zipd
+            </span>{" "}
+            Links
+          </h1>
+      </div>
+    </>
   )
 }
 
